27703bab13fa7b462d83c738f5dff272fe8fdae5
Ross Fisher <zorrobyte@gmail.com>
2019-06-12 12:30:23 -0400

    Rewrote the sketch

--- prius_mag_angle_sensor_CAN/prius_mag_angle_sensor_CAN.ino
+++ prius_mag_angle_sensor_CAN/prius_mag_angle_sensor_CAN.ino
@@ -1,8 +1,13 @@
-#include <SPI.h>
+#include <can.h>
 #include <mcp2515.h>
 #include <ams_as5048b.h>
 
-//unit consts for as5048b
+//CAN
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+MCP2515 mcp2515(10);
+
+//ANGSENSOR
 #define U_RAW 1
 #define U_TRN 2
 #define U_DEG 3
@@ -13,26 +18,8 @@
 #define U_MILNATO 8
 #define U_MILSE 9
 #define U_MILRU 10
-
 AMS_AS5048B angsensor;
-
-//min angle is 0, max is like 16357.00 or around for RAW. Wraps back around to 0 (unsigned)
-
-struct can_frame canMsg1;
-MCP2515 mcp2515(10);
-
-//angle reading
-int16_t encoder1Reading = 0;
-
-//counter
-int counter = 1;
-
-//last angle
-int lastangle = 0;
-
-//big angle
-int32_t bigangle = 0;
-
+int32_t encoder1Reading = 0;
 
 void setup() {
   
@@ -40,52 +27,51 @@ void setup() {
   Serial.begin(115200);
   SPI.begin();
 
-  //init AMS_AS5048B object
-  angsensor.begin();
-  angsensor.setClockWise(true);
-
-  //consider the current position as zero
-  angsensor.setZeroReg();
-  
+  //INIT CAN
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS);
+  mcp2515.setBitrate(CAN_125KBPS, MCP_8MHZ); //8Mhz oscillator
   mcp2515.setNormalMode();
   
-  Serial.println("Example: Write to CAN");
+  //INIT ANGSENSOR
+  angsensor.begin();
+  angsensor.setClockWise(true);
+  angsensor.setZeroReg(); //consider the current pos as zero
 }
 
 void loop() {
 
-  //the angle sensor reading
-  //Serial.print("Angle sensor raw : ");
-  //Serial.println(angsensor.angleR(U_RAW, true));
+  //ANGSENSOR
+  encoder1Reading = angsensor.angleR(U_DEG, true); //READ the ANGSENSOR
 
-  //set the encoder1reading to the angle
-  encoder1Reading = angsensor.angleR(U_DEG, true);
-  //Serial.println(angsensor.getMovingAvgExp());
-  //Serial.println(angsensor.angleRegR());
-  
-  Serial.println(encoder1Reading);
-  //the message
-  canMsg1.can_id  = 0x23;
+  //CAN
+  canMsg1.can_id  = 0x230;
   canMsg1.can_dlc = 8;
-  canMsg1.data[0] = (encoder1Reading >> 16) & 0xFF;
+  canMsg1.data[0] = (encoder1Reading >> 16) & 0xFF; //Bitshift the ANGSENSOR (24 bit, Cabana errors with 32 bit)
   canMsg1.data[1] = (encoder1Reading >> 8) & 0xFF;
   canMsg1.data[2] = (encoder1Reading >> 0) & 0xFF;
-  canMsg1.data[3] = 0x0;
-  canMsg1.data[4] = 0x0;
-  canMsg1.data[5] = 0x0;
-  canMsg1.data[6] = 0x0;
-  //canMsg1.data[7] = 0x0;
-  canMsg1.data[7] = can_cksum (canMsg1, 7, 0x23); //TODO: Implement This
-  
-  //send message
-  mcp2515.sendMessage(&canMsg1);
+  canMsg1.data[3] = 0x00;
+  canMsg1.data[4] = 0x00;
+  canMsg1.data[5] = 0x00;
+  canMsg1.data[6] = 0x00;
+  canMsg1.data[7] = can_cksum (canMsg1.data, 7, 0x230); //Toyota CAN CHECKSUM
 
-  //Serial.println(canMsg1.data[0]);
-  //Serial.println(canMsg1.data[1]);
+  canMsg2.can_id  = 0x220;
+  canMsg2.can_dlc = 8;
+  canMsg2.data[0] = 0x0E;
+  canMsg2.data[1] = 0x00;
+  canMsg2.data[2] = 0x00;
+  canMsg2.data[3] = 0x08;
+  canMsg2.data[4] = 0x01;
+  canMsg2.data[5] = 0x00;
+  canMsg2.data[6] = 0x00;
+  canMsg2.data[7] = 0xA0;
   
-  delay(5); // 200Hz
+  mcp2515.sendMessage(&canMsg1);
+  //mcp2515.sendMessage(&canMsg2);
+
+  Serial.println("Messages sent");
+  
+  delay(100);
 
 }
 
